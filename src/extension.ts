d3d8bbe2bd6642bb302720a2581aac2cc431153c
zenonux <zenonux@pro.com>
2021-11-28 15:48:00 +0800

    f

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { initConfig,syncLocalToRemote } from './commands/oss';
-import Oss from './services/oss';
 import { setContext } from './utils/vscode';
 export function activate(context: vscode.ExtensionContext) {
 	setContext('ossSync.enabled',true);
