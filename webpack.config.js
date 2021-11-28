5c6380c855d9cc447da4d25c72ef8405ef22bc04
zenonux <zenonux@pro.com>
2021-11-28 16:21:17 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ const extensionConfig = {
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
+    extensions: ['.js','.ts',]
   },
   module: {
     rules: [
